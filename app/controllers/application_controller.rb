139a332472106867a29153cd71bd6a286957239a
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-04-29 04:50:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(params[:team])
 
-      params [:team][:members].each do |info|
+      params[:team][:members].each do |info|
         Hero.new(info)
       end
 
